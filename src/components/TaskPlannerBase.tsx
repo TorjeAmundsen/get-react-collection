7f471955a385264fbb2f847d4dbe0f8968e32810
Torje Amundsen <torjefrafen@hotmail.no>
2023-09-01 13:53:28 +0200

    Update TaskPlannerBase.tsx

--- src/components/TaskPlannerBase.tsx
+++ src/components/TaskPlannerBase.tsx
@@ -1,17 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Task {
+  description: string;
+  isDone: boolean;
+  assignedTo: string;
+  doneDate: DateConstructor | undefined;
+  editMode: boolean;
+}
 
 function TaskPlannerBase() {
-  const [tasksArray, setTasksArray] = useState([
+  const [tasks, setTasks] = useState<Task[]>([
     {
-      description: "",
+      description: "Add a task...",
       isDone: false,
-      doneDate: new Date(),
+      assignedTo: "N/A",
+      doneDate: undefined,
       editMode: false,
     },
   ]);
+  const [numTasks, setNumTasks] = useState(1);
+
+  useEffect(() => {}, [numTasks]);
+
+  const handleClick = () => {
+    setTasks((prev) => {
+      prev.push({
+        description: "New task",
+        isDone: false,
+        assignedTo: "Dummy",
+        doneDate: undefined,
+        editMode: false,
+      });
+      console.log(prev);
+      return prev;
+    });
+    setNumTasks((prev) => prev + 1);
+  };
+
   return (
     <>
-      <div className="center">TaskPlanner is under construction.</div>
+      <div className="center">
+        TaskPlanner is under construction.
+        {tasks.map((task) => (
+          <div>
+            {task.description}, {task.assignedTo}
+          </div>
+        ))}
+      </div>
+      <div className="taskplanner-set-task">
+        <button onClick={handleClick}>Add a generated task</button>
+      </div>
     </>
   );
 }
